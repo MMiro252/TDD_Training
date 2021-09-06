3f46eeb3d8cd02fab0a5f6fcd2d3e56d6dd1b020
Miroslav Macura <miroslav.macura@siemens-healthineers.com>
2021-09-06 12:37:37 +0200

    Add project files.

---
+++ TDD_Training.Tests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace TDD_Training.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Test_5MultiplyBy4_Returns20()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Multiply(5, 4);
+            Assert.That(result, Is.EqualTo(20));
+        }
+    }
+}
---
+++ TDD_Training/Calculator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TDD_Training
+{
+    public class Calculator
+    {
+        public double Multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+    }
+}
